1a28b5619705a93bd6cb6fdb565979b925baf281
Leo Brooks <ltbrooks@student.unimelb.edu.au>
2024-01-10 01:37:43 +1100

    List contents of mealtime, cuisines, and drinks added

--- MealGenerator.js
+++ MealGenerator.js
@@ -1,16 +1,6 @@
-/* MAIN IDEA
-
-   A program that generates a message containing a random cuisine and beverage for a select meal of the day.
-
-*/
-
-
-// There will be a List of strings for each of the 3 random components (cuisine, drink, mealtime)
-let mealtimes = [];
-let cuisines = [];
-let drinks = [];
-
-// A String is selected from each list
+let mealtimes = ['Breakfast', 'Lunch', 'Dinner', 'Dessert', 'Midnight Snack', 'Afternoon Tea', 'Brunch', 'Morning Tea', 'Pre-workout Snack', 'Post-workout Snack'];
+let cuisines = ['Italian', 'Chinese', 'Indian', 'Japanese', 'Thai', 'Indonesian', 'Malaysian', 'Mexican', 'American', 'Australian', 'Lebanese'];
+let drinks = ['Water', 'Milk', 'Orange Juice', 'Apple Juice', 'Coca Cola', 'Fanta', 'Lemonade', 'Sprite', 'Beer', 'Iced Tea', 'Iced Coffee'];
 
 function getRandomPosInt(max) {
     return Math.floor(Math.random() * max);
@@ -19,7 +9,5 @@ function getRandomPosInt(max) {
 let mealtime = mealtimes[getRandomPosInt(mealtimes.length)];
 let cuisine = cuisines[getRandomPosInt(cuisines.length)];
 let drink = drinks[getRandomPosInt(drinks.length)];
-
-// Message will be stored and displayed by logging the message to the console through a concatenation of strings
 
 console.log(`Today for your ${mealtime} you will be having amazing ${cuisine} food with a lovely glass of ${drink} to wash it down!`);
