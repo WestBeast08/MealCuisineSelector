07a8a473a952e4ecec7dd0c54aa139f5c973c90e
Leo Brooks <ltbrooks@student.unimelb.edu.au>
2024-01-10 01:14:00 +1100

    Code structure completed

--- MealGenerator.js
+++ MealGenerator.js
@@ -6,7 +6,20 @@
 
 
 // There will be a List of strings for each of the 3 random components (cuisine, drink, mealtime)
+let mealtimes = [];
+let cuisines = [];
+let drinks = [];
 
 // A String is selected from each list
 
+function getRandomPosInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+let mealtime = mealtimes[getRandomPosInt(mealtimes.length)];
+let cuisine = mealtimes[getRandomPosInt(cuisines.length)];
+let drink = mealtimes[getRandomPosInt(drinks.length)];
+
 // Message will be stored and displayed by logging the message to the console through a concatenation of strings
+
+console.log(`${mealtime} ${cuisine} ${drink}`);
