6c41eb25de226af324d7cd8037b2f9e64aa51669
Leo Brooks <ltbrooks@student.unimelb.edu.au>
2024-01-10 01:00:03 +1100

    more planning

--- MealGenerator.js
+++ MealGenerator.js
@@ -3,3 +3,10 @@
    A program that generates a message containing a random cuisine and beverage for a select meal of the day.
 
 */
+
+
+// There will be a List of strings for each of the 3 random components (cuisine, drink, mealtime)
+
+// A String is selected from each list
+
+// Message will be stored and displayed by logging the message to the console through a concatenation of strings
